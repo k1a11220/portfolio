c8724404f62f32897fe450eaddb5d5abf57ae1da
Son Beomsoo <k1a11220@naver.com>
2022-04-04 09:47:07 +0900

    Clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Beomsoo Son</title>
         <meta
@@ -15,9 +13,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main></main>
 
-      <footer className={styles.footer}></footer>
+      <footer></footer>
     </div>
   );
 };
