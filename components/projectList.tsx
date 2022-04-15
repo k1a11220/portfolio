429ed632f910b593efc189cd2f36a174b583e4b4
Son Beomsoo <k1a11220@naver.com>
2022-04-15 22:18:21 +0900

    Add thumbnail image

--- components/projectList.tsx
+++ components/projectList.tsx
@@ -2,6 +2,11 @@ import styled from "@emotion/styled";
 import Image from "next/image";
 import Link from "next/link";
 import Thumbnail_ald from "@media/thumbnail_ald.jpg";
+import Thumbnail_bokdda from "@media/thumbnail_bokdda.png";
+import Thumbnail_goonin from "@media/thumbnail_goonin.png";
+import Thumbnail_solift from "@media/thumbnail_solift.png";
+import Thumbnail_hydro from "@media/thumbnail_hydro.png";
+import Thumbnail_javara from "@media/thumbnail_javara.png";
 
 const ProjectContainer = styled.ul`
   list-style: none;
@@ -28,7 +33,17 @@ export function ProjectList() {
     <ProjectContainer>
       <Project>
         <Link href="/projects/ald">
-          <Image src={Thumbnail_ald} layout="responsive"></Image>
+          <Image src={Thumbnail_solift} layout="responsive"></Image>
+        </Link>
+      </Project>
+      <Project>
+        <Link href="/projects/ald">
+          <Image src={Thumbnail_bokdda} layout="responsive"></Image>
+        </Link>
+      </Project>
+      <Project>
+        <Link href="/projects/ald">
+          <Image src={Thumbnail_goonin} layout="responsive"></Image>
         </Link>
       </Project>
       <Project>
@@ -38,22 +53,12 @@ export function ProjectList() {
       </Project>
       <Project>
         <Link href="/projects/ald">
-          <Image src={Thumbnail_ald} layout="responsive"></Image>
+          <Image src={Thumbnail_hydro} layout="responsive"></Image>
         </Link>
       </Project>
       <Project>
         <Link href="/projects/ald">
-          <Image src={Thumbnail_ald} layout="responsive"></Image>
-        </Link>
-      </Project>
-      <Project>
-        <Link href="/projects/ald">
-          <Image src={Thumbnail_ald} layout="responsive"></Image>
-        </Link>
-      </Project>
-      <Project>
-        <Link href="/projects/ald">
-          <Image src={Thumbnail_ald} layout="responsive"></Image>
+          <Image src={Thumbnail_javara} layout="responsive"></Image>
         </Link>
       </Project>
     </ProjectContainer>
---
+++ pages/projects/bokdda.tsx
@@ -0,0 +1,33 @@
+import { CustomHead } from "@components/customHead";
+import { ProjectHeader } from "@components/project/projectHeader";
+import { Divider, Section } from "@components/typography";
+import { NextPage } from "next";
+import thumbnail from "@media/thumbnail_bokdda.jpg";
+
+const BokddaPage: NextPage = () => {
+  return (
+    <>
+      <CustomHead
+        title="ALD Equipments develop project"
+        description="How we reduced friction in one of the identity confirmation flow of Facebook"
+      />
+      <article>
+        <ProjectHeader
+          title="ALD Equipments develop project"
+          description="How we reduced friction in one of the identity confirmation flow of Facebook"
+          timeline="Dec 2018 - Jan 2019"
+          role="Design Engineering & Research"
+          profile={[
+            { name: "Beyonghun", role: "Design Engineering", image: tngLogo },
+            { name: "Wonjun", role: "Research, Test", image: kitechLogo },
+          ]}
+          thumbnail={thumbnail}
+        />
+        <Divider />
+        <Section></Section>
+      </article>
+    </>
+  );
+};
+
+export default BokddaPage;
