0a378262c2f47a920fb3388ec90301a9ae8a2730
Son Beomsoo <k1a11220@naver.com>
2022-08-31 20:19:33 +0900

    chore: Edit solift product link

--- pages/projects/solift/index.tsx
+++ pages/projects/solift/index.tsx
@@ -8,6 +8,7 @@ import img_1 from "@media/solift/thumbnail-3.png";
 
 import ComingSoon from "@components/project/coming-soon";
 import StyledImage from "@components/project/styled-image";
+import FloatingButton from "@components/floating-button";
 
 const SoliftPage: NextPage = () => {
   return (
@@ -17,8 +18,14 @@ const SoliftPage: NextPage = () => {
         <ProjectHeader
           title="SOLIFT, OKR 기반의 개인목표 관리 서비스"
           description="직접 복무하면서 느낀 불편함을 바탕으로 병사들의 의미있는 군생활을 도와주는 솔루션을 만듭니다."
-          timeline="May 2022 - Present"
+          timeline="May 2022 - July 2022"
           role="프로덕트 디자인 & 풀스택 개발"
+          product={[
+            {
+              type: "ios",
+              href: "https://apps.apple.com/kr/app/solift/id1637383448",
+            },
+          ]}
           hero={hero}
           background={"#F4FAFF"}
         />
@@ -26,7 +33,11 @@ const SoliftPage: NextPage = () => {
         {/* <Section>
           <StyledImage src={img_1} />
         </Section> */}
-        <ComingSoon type="coming" />
+        <FloatingButton
+          icon={"bulb"}
+          label={"제품 써보기"}
+          link="https://apps.apple.com/kr/app/solift/id1637383448"
+        />
       </article>
     </>
   );
